8d84c2b15430dec10995316ed7d52b5ac7acc222
mercurylamp1 <122327990+mercurylamp1@users.noreply.github.com>
2024-08-25 05:10:14 +0900

    forgor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,10 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 
+		dependencies = {
+			"p00f/clangd_extensions.nvim"
+		},
+
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			require'lspconfig'.clangd.setup({
@@ -15,11 +19,26 @@ return {
 					"--enable-config",
 					"--pch-storage=disk",
 					"--header-insertion-decorators",
+					"--query-driver",
+					"gcc",
 				},
 				capabilities = capabilities,
 			})
+			require'lspconfig'.zls.setup{
+				cmd = {
+					"zls",
+				},
+
+				settings = {
+					zls = {
+					},
+				},
+				capabilities = capabilities,
+			}
+
 			vim.keymap.set('n', '<leader>h', vim.cmd.ClangdSwitchSourceHeader, {desc = "switch src/header"})
 			vim.keymap.set('n', '<C-n>', function() vim.lsp.buf.hover() end, {desc = "hover"})
 		end
 	}
 }
+
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 
 		config = function()
 			require'nvim-treesitter.configs'.setup {
-				ensure_installed = { "c", "cpp", "lua","vim", "vimdoc"},
+				ensure_installed = { "c", "cpp", "lua","vim", "vimdoc", "zig"},
 				sync_install = false,
 				auto_install = true,
 				highlight = {
